6023447be4c6967b0148f2755ff0cf2f5b67e6c1
Gaowater <3384357097@qq.com>
2026-06-11 00:06:05 +0800

    纪念日新增3月22日-你递给我的第一杯奶茶

--- src/config/memorableDatesConfig.ts
+++ src/config/memorableDatesConfig.ts
@@ -17,5 +17,6 @@ export interface MemorableDate {
 export const memorableDates: MemorableDate[] = [
   // ⬇️ 在这里添加你的纪念日 ⬇️
   { date: "2026-02-07", label: "相识" },
+  { date: "2026-03-22", label: "你递给我的第一杯奶茶" },
   // { date: "2026-XX-XX", label: "纪念日" },
 ];
