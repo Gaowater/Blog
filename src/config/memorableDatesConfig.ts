93dd9f6949269f6a556a57016910d91d69d353b4
Gaowater <3384357097@qq.com>
2026-06-18 00:17:16 +0800

    feat: 新增6.10第一次煲电话粥纪念日

--- src/config/memorableDatesConfig.ts
+++ src/config/memorableDatesConfig.ts
@@ -23,5 +23,6 @@ export const memorableDates: MemorableDate[] = [
   { date: "2026-05-17", label: "一百天纪念日" },
   { date: "2026-06-12", label: "表白日" },
   { date: "2026-06-13", label: "第一次深度聊天" },
+  { date: "2026-06-10", label: "第一次煲电话粥" },
   // { date: "2026-XX-XX", label: "纪念日" },
 ];
