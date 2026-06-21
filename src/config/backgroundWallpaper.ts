3a8fe3709a89e142e2a64a2fb311e19af40aa954
Gaowater <3384357097@qq.com>
2026-06-21 19:09:59 +0800

    small

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -74,7 +74,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否允许用户通过控制面板切换横幅标题显示
 			switchable: true,
 			// 主页横幅主标题
-			title: "你好啊！依琳~",
+			title: "你好啊！我最喜欢的宝~",
 			// 主页横幅主标题字体大小
 			titleSize: "3.8rem",
 			// 主页横幅副标题
