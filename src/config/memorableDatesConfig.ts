303e799f72da865c505531882e7fbdd9f74b1ca5
Gaowater <3384357097@qq.com>
2026-09-09 22:44:14 +0800

    chore: 更新内容

--- src/config/memorableDatesConfig.ts
+++ src/config/memorableDatesConfig.ts
@@ -35,5 +35,6 @@ export const memorableDates: MemorableDate[] = [
   { date: "2026-08-05", label: "第一次和你拼豆" },
   { date: "2026-08-13", label: "手帐诞生" },
   { date: "2026-08-25", label: "两百天纪念日" },
+  { date: "2026-09-07", label: "第一次去健身房健身" },
   // { date: "2026-XX-XX", label: "纪念日" },
 ];
